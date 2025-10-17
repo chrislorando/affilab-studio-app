ee82a3235baf2c627df378c4172822951556bc39
CML <chrismanuel.lorando@gmail.com>
2025-10-18 03:02:22 +0700

    refactor ExampleTest to improve test method naming and structure

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,9 +9,16 @@ class ExampleTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_returns_a_successful_response(): void
+    public function test_home_redirects_to_dashboard(): void
     {
         $response = $this->get(route('home'));
+
+        $response->assertRedirect(route('dashboard'));
+    }
+
+    public function test_login_page_returns_successful_response(): void
+    {
+        $response = $this->get(route('login'));
 
         $response->assertStatus(200);
     }
